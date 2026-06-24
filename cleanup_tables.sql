2ad0cdc8ff68c7203326af8dc1cf018acac19f51
Nasser Gasmi <nasser.Gasmi@gmail.com>
2026-06-24 07:07:21 +0200

    fix: session persistante entre onglets via SessionProvider

--- cleanup_tables.sql
+++
@@ -1,9 +0,0 @@
--- Supprimer toutes les tables sauf Pongori, Cornilleau, Joola, Butterfly
-DELETE FROM tables_tt
-WHERE marque NOT IN ('Pongori', 'Cornilleau', 'Joola', 'Butterfly');
-
--- Vérification
-SELECT marque, COUNT(*) AS nb
-FROM tables_tt
-GROUP BY marque
-ORDER BY marque;
